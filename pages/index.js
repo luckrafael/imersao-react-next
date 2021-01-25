5405079dcfa25359dd04a0933348719569bfdb54
luckrafael <lrs.rafael@hotmail.com>
2021-01-25 11:59:54 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+function Title(props) {
+  return <h1>Hello World!</h1>
+}
 
 export default function Home() {
   return <Title>My page</Title>
