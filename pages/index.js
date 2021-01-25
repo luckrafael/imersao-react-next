a66a458f13ffc13442ea363239a2a8776c34eaae
luckrafael <lrs.rafael@hotmail.com>
2021-01-25 13:53:52 -0300

    project structuring

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,46 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget'
 
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `
 
-function Title(props) {
-  return <h1>Hello World!</h1>
-}
+const BackgroundImage = styled.div `
+    background-image: url(${db.bg});
+    background-size: cover;
+    flex: 1;
+    background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Teste</h1>
+          </Widget.Header>
+          <Widget.Content>   
+            <p>Lorem ipsum dolor sit amet..</p>
+          </Widget.Content>
+        </Widget>
+          
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+            <p>Lorem ipsum dolor sit amet..</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  );
 }
